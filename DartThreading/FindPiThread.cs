67b5bc3affdfa1c638824bbf5ca482ad43b4d61d
Anthony Anderson <antgande@ut.utm.edu>
2020-10-27 03:47:49 -0500

    Wrote and tested code, wrote comments and headers

---
+++ DartThreading/FindPiThread.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Arthur: Tony Anderson
+ * Date: 10/27/2020
+ * Filename: FindPiThread.cs
+ * Description: This is a Thread class this is used by the program
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DartThreading
+{
+    class FindPiThread
+    {
+        private int darts;
+        private int hits;
+        Random throws;
+
+        //Accesor for main
+        public int hitsAccesor { get => hits; }
+
+        //Parameterized Constructor
+        public FindPiThread(int darts)
+        {
+            this.darts = darts;
+            hits = 0;
+            throws = new Random();
+        }
+
+        //Running Thread
+        public void throwDarts()
+        {
+            for (int i = 0; i < darts; i++)
+            {
+                double x = throws.NextDouble();
+                double y = throws.NextDouble();
+                x *= x;
+                y *= y;
+
+                double guess = Math.Sqrt(x + y);
+
+                if (guess <= 1)
+                {
+                    hits += 1;
+                }
+
+            }
+        }
+    }
+}
--- DartThreading/Program.cs
+++ DartThreading/Program.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/*
+ * Arthur: Tony Anderson
+ * Date: 10/27/2020
+ * Filename: Program.cs
+ * Description: This is a program that 
+ * estimates the value of pi using the monte carlo method
+ * by throwing darts and use the ratio of darts that hit the
+ * target
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace DartThreading
 {
@@ -6,7 +19,56 @@ namespace DartThreading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int totaldarts = 0;
+            int hits = 0;
+
+            Console.WriteLine("How many darts do you want to throw at the board?");
+            string string1  = Console.ReadLine();
+            Console.WriteLine("How many threads do you want to throw your darts at?");
+            string string2 = Console.ReadLine();
+
+            int userDarts;
+            int userThreads;
+
+            int.TryParse(string1, out userDarts);
+            int.TryParse(string2, out userThreads);
+
+            //Threads
+            List<Thread> threadslist = new List<Thread>(userThreads);
+            List<FindPiThread> Pithreadslist  = new List<FindPiThread>(userThreads);
+
+            //Setting up threads
+            for(int i = 0; i < userThreads; i++)
+            {
+                FindPiThread piThread = new FindPiThread(userDarts);
+                Pithreadslist.Add(piThread);
+                Thread myThread = new Thread(new ThreadStart(piThread.throwDarts));
+                threadslist.Add(myThread);
+                myThread.Start();
+                Thread.Sleep(16);
+            }
+
+            //Waiting
+            for(int i = 0; i < threadslist.Count; i++)
+            {
+                threadslist[i].Join();
+            }
+
+            //Collecting Data
+            for(int i = 0; i < Pithreadslist.Count; i++)
+            {
+                hits += Pithreadslist[i].hitsAccesor;
+            }
+
+            
+            totaldarts = userDarts * userThreads;
+            double piEvaluation = 4 * ((double)hits / (double)totaldarts);
+
+            //Results
+            Console.WriteLine($"The number of darts that hit are {hits}" );
+            Console.WriteLine($"The total darts thrown at the target were {totaldarts}");
+            Console.WriteLine($"The evaluation of Pi was {piEvaluation} ");
+
         }
     }
 }
